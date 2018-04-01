ec795762e5a9d606d82d0bb1996d048cee27f37b
Shailendra53 <2016csb1059@iitrpr.ac.in>
2018-04-01 16:13:48 +0530

    delete

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	//if($_SERVER["REQUEST_METHOD"] == "POST"){
-		$con = mysqli_connect("localhost","root","root","newsy") or die(mysqli_error($con));
-		session_start();
-		session_unset();
-		session_destroy();
-		header("Location: login.php");
-?>
