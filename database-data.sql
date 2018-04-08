be46ca9f175f2bb2a8624705241ec3546e5544d1
abhi <2016csb1026@iitrpr.ac.in>
2018-04-08 12:36:04 +0530

    sql_quries

---
+++ database-data.sql
@@ -0,0 +1,35 @@
+use ezdoc;
+
+INSERT INTO category (category_name) VALUES ("spray");
+INSERT INTO category (category_name) VALUES ("liquid");
+INSERT INTO category (category_name) VALUES ("tablets");
+INSERT INTO medicine (medicine_name,price,category_id) VALUES ("moov spray",100,1);
+INSERT INTO medicine (medicine_name,price,category_id) VALUES ("nosal spray",150,1);
+INSERT INTO medicine (medicine_name,price,category_id) VALUES ("odonil spray",70,1);
+INSERT INTO medicine (medicine_name,price,category_id) VALUES ("aspray",100,1);
+INSERT INTO medicine (medicine_name,price,category_id) VALUES ("b spray",150,1);
+INSERT INTO medicine (medicine_name,price,category_id) VALUES ("cspray",70,1);
+INSERT INTO medicine (medicine_name,price,category_id) VALUES ("dspray",100,1);
+INSERT INTO medicine (medicine_name,price,category_id) VALUES ("e spray",150,1);
+INSERT INTO medicine (medicine_name,price,category_id) VALUES ("f spray",70,1);
+INSERT INTO medicine (medicine_name,price,category_id) VALUES ("moov liquid",200,2);
+INSERT INTO medicine (medicine_name,price,category_id) VALUES ("nosal liquid",250,2);
+INSERT INTO medicine (medicine_name,price,category_id) VALUES ("odonil liquid",70,2);
+INSERT INTO medicine (medicine_name,price,category_id) VALUES ("aliquid",200,2);
+INSERT INTO medicine (medicine_name,price,category_id) VALUES ("b liquid",250,2);
+INSERT INTO medicine (medicine_name,price,category_id) VALUES ("cliquid",70,2);
+INSERT INTO medicine (medicine_name,price,category_id) VALUES ("dliquid",200,2);
+INSERT INTO medicine (medicine_name,price,category_id) VALUES ("e liquid",250,2);
+INSERT INTO medicine (medicine_name,price,category_id) VALUES ("f liquid",70,2);
+INSERT INTO medicine (medicine_name,price,category_id) VALUES ("moov tablets",100,1);
+INSERT INTO medicine (medicine_name,price,category_id) VALUES ("nosal tablets",150,1);
+INSERT INTO medicine (medicine_name,price,category_id) VALUES ("odonil tablets",70,1);
+INSERT INTO medicine (medicine_name,price,category_id) VALUES ("atablets",100,1);
+INSERT INTO medicine (medicine_name,price,category_id) VALUES ("b tablets",150,1);
+INSERT INTO medicine (medicine_name,price,category_id) VALUES ("ctablets",70,1);
+INSERT INTO medicine (medicine_name,price,category_id) VALUES ("dtablets",100,1);
+INSERT INTO medicine (medicine_name,price,category_id) VALUES ("e tablets",150,1);
+INSERT INTO medicine (medicine_name,price,category_id) VALUES ("f tablets",70,1);
+
+
+
---
+++ database-schema.sql
@@ -0,0 +1,46 @@
+DROP database IF EXISTS ezdoc; 
+CREATE database ezdoc;
+USE ezdoc;
+
+
+CREATE TABLE category(
+	category_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+	category_name VARCHAR(45) NOT NULL,
+	PRIMARY KEY (category_id)
+);
+
+CREATE TABLE medicine(
+	medicine_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+	medicine_name VARCHAR(45) NOT NULL,
+	category_id SMALLINT UNSIGNED,
+	price FLOAT UNSIGNED,
+	PRIMARY KEY  (medicine_id),
+	FOREIGN KEY (category_id) REFERENCES category(category_id) ON DELETE cascade ON UPDATE CASCADE
+);
+
+CREATE TABLE city(
+	city_id  SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT, 
+	city_name varchar(20) NOT NULL,
+	PRIMARY KEY (city_id)
+);
+
+CREATE TABLE shop(
+	shop_id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+	address varchar(30) NOT NULL,
+	city_id SMALLINT UNSIGNED NOT NULL,
+	PRIMARY KEY(shop_id),
+	FOREIGN KEY (city_id) REFERENCES city(city_id) ON DELETE cascade ON UPDATE CASCADE
+);
+
+CREATE TABLE shop_medicine(
+	shop_id SMALLINT UNSIGNED,
+	medicine_id SMALLINT UNSIGNED,
+	quantity SMALLINT UNSIGNED DEFAULT 0,
+	FOREIGN KEY (shop_id) REFERENCES shop(shop_id)  ON DELETE cascade ON UPDATE CASCADE,
+	FOREIGN KEY (medicine_id) REFERENCES medicine(medicine_id)  ON DELETE cascade ON UPDATE CASCADE
+);
+
+
+	
+
+
