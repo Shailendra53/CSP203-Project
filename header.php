549b9b8c6afe295b3fb63bea7e5764995d24ac3e
MeghanaBatchu <2016csb1034@iitrpr.ac.in>
2018-04-16 08:50:08 +0530

    Update header.php

--- header.php
+++ header.php
@@ -2,15 +2,7 @@
 	require('connect.php');
 	if (@$_SESSION['username']) {
 ?>
-<center><a href="index.php">Discussion Forum</a> | <a href="account.php">My account</a> | <a href="members.php">Members</a> | 
-<?php 
-$check = mysqli_query($connect,"SELECT * FROM users WHERE username = '".$_SESSION['username']."'");
-	$rows = mysqli_num_rows($check);
-	while ($row = mysqli_fetch_assoc($check)) {
-		$id = $row['id'];
-	}
-echo "<a href='profile.php?id=$id'>".@$_SESSION["username"]; 
-?></a>
+<center><a href="index.php">Discussion Forum</a> | <a href="account.php">My account</a> | <a href="members.php">Members</a>
 | <a href="index.php?action=logout"> logout</a></center>
 <?php
 	}
